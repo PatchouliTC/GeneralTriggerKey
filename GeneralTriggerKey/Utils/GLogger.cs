aadaa601abc18c0077ff3d297deba6cddbdc73c0
PatchouliTC <1009609373@qq.com>
2023-04-09 21:41:36 +0800

    添加项目文件。

---
+++ GeneralTriggerKey/Utils/GLogger.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+
+namespace GeneralTriggerKey.Utils
+{
+    public class GLogger
+    {
+        private ILoggerFactory _factory = NullLoggerFactory.Instance;
+
+        private static GLogger s_instance = default!;
+
+        public static GLogger Instance
+        {
+            get
+            {
+                return s_instance ??= new GLogger();
+            }
+        }
+        private GLogger() { }
+
+
+        public ILoggerFactory GlobalLoggerFactory
+        {
+            get { return _factory; }
+        }
+
+        public bool IsNullLogger
+        {
+            get
+            {
+                return _factory is NullLoggerFactory;
+            }
+        }
+
+        public void SetFactory(ILoggerFactory _factory)
+        {
+            this._factory = _factory;
+        }
+        public ILogger GetLogger<T>()
+        {
+            return _factory.CreateLogger<T>();
+        }
+        public ILogger GetLogger(string name)
+        {
+            return _factory.CreateLogger(name);
+        }
+        public ILogger GetLogger(Type type)
+        {
+            return _factory.CreateLogger(type);
+        }
+    }
+}
---
+++ GeneralTriggerKey/Utils/IdCreator.cs
@@ -0,0 +1,66 @@
+﻿using IdGen;
+using System;
+
+namespace GeneralTriggerKey.Utils
+{
+    public class IdCreator
+    {
+        private IdGenerator _generator;
+        private long require_times = 0;
+        private static IdCreator s_instance = default!;
+        public static IdCreator Instance
+        {
+            get
+            {
+                return s_instance ??= new IdCreator();
+            }
+        }
+
+        private IdCreator()
+        {
+            _generator = new IdGenerator(0, new IdGeneratorOptions(timeSource: new DefaultTimeSource(DateTime.UtcNow)));
+        }
+
+        public DateTimeOffset IdTimeSeed
+        {
+            get => _generator.Options.TimeSource.Epoch;
+        }
+        public int GeneratorId
+        {
+            get => _generator.Id;
+        }
+        public bool IsGenedIds
+        {
+            get => require_times > 0;
+        }
+
+        /// <summary>
+        /// 创建自定义配置的ID生成器
+        /// </summary>
+        /// <param name="init_seed"></param>
+        /// <param name="time_seed"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public void SetCustomGenerator(int init_seed, in DateTime time_seed)
+        {
+            if (IsGenedIds)
+                throw new InvalidOperationException("Id generator already be used,reset config will be cause duplicate id happened");
+
+            _generator = new IdGenerator(init_seed, new IdGeneratorOptions(timeSource: new DefaultTimeSource(time_seed)));
+        }
+
+        /// <summary>
+        /// 创建ID
+        /// </summary>
+        /// <returns></returns>
+        public long GetId()
+        {
+            require_times += 1;
+            return _generator.CreateId();
+        }
+
+        public override string ToString()
+        {
+            return $"IdGenerator Details:\nSeed:{_generator.Id}\nTimeSeed:{_generator.Options.TimeSource.Epoch}\nGenerate Times:{require_times}\nGenSpeed/ms:{_generator.Options.IdStructure.MaxGenerators * _generator.Options.IdStructure.MaxSequenceIds}";
+        }
+    }
+}
