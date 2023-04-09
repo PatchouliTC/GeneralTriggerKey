c3baea887dcad890ecc5e8e653f7ebad5cc98921
PatchouliTC <1009609373@qq.com>
2023-04-10 00:10:03 +0800

    - add benchmark

---
+++ GeneralTriggerKey.Benchmark/CodeBenchmark.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using GeneralTriggerKey.Attributes;
+using static GeneralTriggerKey.Operator;
+
+namespace GeneralTriggerKey.Benchmark
+{
+    [MapEnum]
+    public enum MapTestEnum
+    {
+        [EnumAlia("testA")]
+        A = 1,
+        [EnumAlia("testB", "TESTB")]
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6,
+        G = 7,
+        H = 8,
+        宣言,
+        选择,
+        发动,
+    }
+    [MemoryDiagnoser]
+    public class CodeBenchmark
+    {
+        public CodeBenchmark()
+        {
+            InjectEnumsFromAssembly(typeof(CodeBenchmark).Assembly);
+        }
+        [Benchmark]
+        public void TestSameCodeAdd()
+        {
+            var key = Q(MapTestEnum.F)&Q(MapTestEnum.H);
+
+            _ = Q("testA") * key;
+        }
+    }
+}
---
+++ GeneralTriggerKey.Benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Running;
+using GeneralTriggerKey.Attributes;
+using GeneralTriggerKey.Benchmark;
+using GeneralTriggerKey.KeyMap;
+using GeneralTriggerKey.Utils;
+using GeneralTriggerKey.Utils.Extensions;
+using Microsoft.Extensions.Logging;
+using static GeneralTriggerKey.Operator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //GLogger.Instance.SetFactory(new NLogLoggerFactory());
+        GLogger.Instance.SetFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+        {
+            GLogger.Instance.GetLogger<Program>().LogCritical(e.ExceptionObject as Exception, "CriticalHappened");
+        };
+
+        InjectEnumsFromAssembly(typeof(Program).Assembly);
+
+        var summary = BenchmarkRunner.Run<CodeBenchmark>();
+    }
+}
